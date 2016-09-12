80b0d55c2c8756b7e5d9db38b777d91f546dbc57
Bianca <bianca@jawsdigital.com.br>
2016-09-12 08:25:27 -0300

    Adicionando arquivos

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Jaws Digital Internet">
+    <meta name="keywords" content="7,7Seven,Sete,SeteSeven,LP,Landing,Page,Seven,Transporte,Aéreo,Rodoviário,Cargas,Logística">
+
+	<!--CSS BOOTSTRAP-->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--CSS-->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<title>7 Seven</title>
+</head>
+<body>
+<!--Topo 7Seven-->
+	<div class="bg-topo">
+		<header>
+			<div class="container">	
+			<!--Row-->	
+				<div class="row contato-row">
+					<div class="col-sm-4 contato">
+						<img src="assets/img/mouse.png"><span>setesevenlogbrasil.com.br</span><br><br>
+						<img src="assets/img/envelope.png"><span>contato@setesevenlogbrasil.com.br</span>
+					</div>
+					<div class="col-sm-4 logo">
+						<img src="assets/img/logo.png">
+					</div>
+					<div class="col-sm-4 telefone">
+						<span>11 <b>2541.6804</b></span><br>
+						<span>11 <b>2545.1637</b></span><br>
+						<img src="assets/img/whatsapp.png"><span>11 <b>9.4792.4848</b></span><br>
+					</div>
+				</div>	
+				<!--Fim Row-->
+				<!--Frases-->					
+				<div class="row">
+					<div class="col-sm-12 text-center text-uppercase frase">
+						<span class="frase-sm"><b>A menor distância entre dois pontos</b></span>
+						<span class="frase-destaque"><b>15 anos de experiência em <br><span class="laranja">transporte aéreo</span> e <span class="laranja">rodoviário de cargas</span>,<br> para todo território nacional</b></span>
+					</div>
+				</div>
+				<!--Fim Row-->	
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<img src="assets/img/caixas1.png">						
+					</div>
+				</div>
+				<!--Fim Row-->
+			</div>
+		</header>
+	</div>
+	<!-- Fim topo 7SEVEN-->
+</body>
+</html>
